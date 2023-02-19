0d3d157e0c13362c981cbee938a0d9a0ec7e8175
mikolanny <mikolany@yahoo.com>
2023-02-18 17:10:03 -0800

    llp

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *size off - checker
+ *
+ *Main entry point
+ *
+ *Return always 0 (success)
+ */
+
+main()
+
+{
+	char           ch = ' ';
+	int            int_num = 0;
+	float          flt_num = 0.0f;
+	long int       long_int_num = 0;
+	long long int  long_long_int_num = 0;
+
+printf("sizeof a  char is : %d - byte\n", sizeof(char));
+printf("sizeof an int  is : %d - byte\n", sizeof(int));
+printf("sizeof a  float is : %d - byte\n", sizeof(float));
+printf("sizeof a  long int is : %d - byte\n", sizeof(long int));
+printf("sizeof a  long long int is : %d - byte\n", sizeof(long long int));
+	return (0);
+
+}

ab4dedb61a6b399cca3e679fa4cc0d8cdb7e70aa
mikolanny <mikolany@yahoo.com>
2023-02-18 17:24:04 -0800

    LLP

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ main()
 	long int       long_int_num = 0;
 	long long int  long_long_int_num = 0;
 
-printf("sizeof a  char is : %d byte(s)\n", sizeof(char));
-printf("sizeof an int  is : %d byte(s)\n", sizeof(int));
-printf("sizeof a  float is : %d byte(s)\n", sizeof(float));
-printf("sizeof a  long int is : %d byte(s)\n", sizeof(long int));
-printf("sizeof a  long long int is : %d byte(s)\n", sizeof(long long int));
+printf("Size of a char is: %d byte(s)\n", sizeof(char));
+printf("Size of an int  is: %d byte(s)\n", sizeof(int));
+printf("Size of a float is: %d byte(s)\n", sizeof(float));
+printf("Size of a long int is: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int is: %d byte(s)\n", sizeof(long long int));
 	return (0);
 
 }

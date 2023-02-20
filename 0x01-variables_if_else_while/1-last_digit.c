23e33b13f42c2800a052007dcf0da948e0a9c6cd
mikolanny <mikolany@yahoo.com>
2023-02-19 17:53:53 -0800

    IfElseWhile

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - enter point
+ *Return: 0(Success)
+ */
+int main(void)
+{
+	int n;
+	int lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
+	if (lastDigit < 6 && lastDigit != 0)
+	{
+		printf("Last digit of %d is %d", n, lastDigit);
+		printf(" and is less than 6 and not 0\n");
+	}
+	else if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains files that use the if else while on C

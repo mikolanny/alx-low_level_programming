59695ea1906672ef17b61abd412b9bc3570d879a
mikolanny <mikolany@yahoo.com>
2023-02-22 20:43:19 -0800

    FNL

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,13 +9,13 @@ void times_table(void)
 	int b;
 
 	for (a = 0; a < 10; a++)
-
 	{
 		for (b = 0; b < 10; b++)
 		{
 			int val = a * b;
 
 			if (b == 0)
+
 				_putchar('0');
 			if (val < 10 && b != 9 && b != 0)
 			{

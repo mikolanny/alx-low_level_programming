17d80e4729126d4b3449b2ab0f300a46404dfd12
mikolanny <mikolany@yahoo.com>
2023-02-22 20:36:43 -0800

    FNL

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,39 +9,39 @@ void times_table(void)
 	int b;
 
 	for (a = 0; a < 10; a++)
-
 	{
 		for (b = 0; b < 10; b++)
 		{
 			int val = a * b;
 
 			if (b == 0)
-
 				_putchar('0');
 			if (val < 10 && b != 9 && b != 0)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + val);
-			_putchar(',');
-		}
-		else if (val >= 10 && b != 9 && b != 0)
-		{
-			_putchar(' ');
-			_putchar('0' + (val / 10));
-			_putchar('0' + (val % 10));
-			_putchar(',');
-		}
-		else if (b == 9 && val < 10)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + val);
-		}
-		else if (b == 9 && val >= 10)
-		{
-			_putchar(' ');
-			_putchar('0' + (val / 10));
-			_putchar('0' + (val % 10));
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + val);
+				_putchar(',');
+			}
+			else if (val >= 10 && b != 9 && b != 0)
+			{
+				_putchar(' ');
+				_putchar('0' + (val / 10));
+				_putchar('0' + (val % 10));
+				_putchar(',');
+			}
+			else if (b == 9 && val < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + val);
+			}
+			else if (b == 9 && val >= 10)
+			{
+				_putchar(' ');
+				_putchar('0' + (val / 10));
+				_putchar('0' + (val % 10));
+			}
 		}
 	}
+}

0180f389e28843a23e4d8039ee8eb180140c115a
mikolanny <mikolany@yahoo.com>
2023-02-23 15:57:44 -0800

    FNL2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+void more_numbers(void);
+
+/**
+ *more_numbers - order from 1 to 14 ten times
+ *
+ *Return: 10x  number list
+ */
+
+void more_numbers(void)
+{
+
+	char i, j, k;
+
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		k = '9';
+		for (i = '0'; i <= '1'; i++)
+		{
+
+			for (j = '0'; j <= k; j++)
+			{
+				if (i > '0')
+
+					_putchar(i);
+				_putchar(j);
+			}
+			k = '4';
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_line- prints line
+ *@n: checked
+ *Return: nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}

66b8f97d16b9793832ba10272e7e637fc968924b
mikolanny <olaniyanlanny@gmail.com>
2023-04-08 10:02:37 -0700

    replaced a while loop

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,28 +1,29 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * binary_to_uint - convert a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- *
- * Return: the converted number, or 0 if there is one or more chars
- * in the string b that is not 0 or 1, or if b is NULL
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 & 1 chars
+ * Return: converted number or 0 if b is NULL or contains chars not 0 or 1.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int bin;
+	unsigned int num = 0, mult = 1;
+	int len = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (bin = 0; b[bin] != '\0'; bin++)
+	while (b[len])
 	{
-		if (b[bin] != '0' && b[bin] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		num <<= 1;
-		if (b[bin] == '1')
-			num += 1;
+		len++;
+	}
+
+	for (len--; len >= 0; len--)
+	{
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
 
 	return (num);

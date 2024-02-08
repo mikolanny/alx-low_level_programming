b6deff530df8152e45155d4f4edf5baa484ba323
Michael Olaniyan <103427098+mikolanny@users.noreply.github.com>
2024-02-08 05:34:44 +0100

    Add files via upload

---
+++ 0x1E-search_algorithms/listint/search_algos.h
@@ -0,0 +1,32 @@
+#ifndef __SEARCH__
+#define __SEARCH__
+
+#include <stdio.h>
+#include <math.h>
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+
+#endif
